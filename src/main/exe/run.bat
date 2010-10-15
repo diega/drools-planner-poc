3f800c5c7e6cd5f6bba379ee33a8c95262d5edfb
Diego López León <dieguitoll@gmail.com>
2010-10-15 20:17:28 -0300

    changed executable files generation for distro

--- src/main/exe/run.bat
+++ src/main/exe/run.bat
@@ -1,1 +1,1 @@
-java -Dlog4j.configuration=file:log4j.xml -jar libs/drools-planner-poc-0.0.1-SNAPSHOT.jar %1 %2
+java -Dlog4j.configuration=file:log4j.xml -jar libs/${project.artifactId}-${project.version}.jar %1 %2
--- src/main/exe/run.sh
+++ src/main/exe/run.sh
@@ -1,1 +1,1 @@
-java -Dlog4j.configuration=file:log4j.xml -jar libs/drools-planner-poc-0.0.1-SNAPSHOT.jar $@
+java -Dlog4j.configuration=file:log4j.xml -jar libs/${project.artifactId}-${project.version}.jar $@
