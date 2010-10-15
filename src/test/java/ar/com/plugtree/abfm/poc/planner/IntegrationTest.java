d9d119055e196c17eb1a5ecded674e769c6b0457
Diego López León <dieguitoll@gmail.com>
2010-10-15 20:11:57 -0300

    ignoring test

--- src/test/java/ar/com/plugtree/abfm/poc/planner/IntegrationTest.java
+++ src/test/java/ar/com/plugtree/abfm/poc/planner/IntegrationTest.java
@@ -1,10 +1,15 @@
 package ar.com.plugtree.abfm.poc.planner;
 
+import org.junit.Ignore;
+import org.junit.Test;
+
 import ar.com.plugtree.abfm.poc.parser.ServiceDeliveryParser;
 import ar.com.plugtree.abfm.poc.parser.TechnicianParser;
 
 public class IntegrationTest {
 
+    @Test
+    @Ignore
     public void readFromFileAndSolve() {
         TechnicianParser technicianParser = new TechnicianParser( getClass().getResourceAsStream( "/technicians.dat" ) );
         ServiceDeliveryParser serviceDeliveryParser = new ServiceDeliveryParser( getClass().getResourceAsStream( "/services.dat" ) );
